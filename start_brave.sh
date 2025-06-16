fea23cc43937756525fe6b727a7168f7a38261ae
susah-diutarakan <158824777+susah-diutarakan@users.noreply.github.com>
2025-06-17 01:59:15 +0700

    Create start_brave.sh

---
+++ start_brave.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+USER=root tightvncserver :1 -geometry 1900x900 -depth 24 &
+sleep 3
+#/opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080 &
+/opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080 --cert /opt/novnc/novnc.crt --key /opt/novnc/novnc.key & #SSL
