a05b6566ee59155ff6eca7b03b749448a4994316
susah-diutarakan <158824777+susah-diutarakan@users.noreply.github.com>
2025-06-17 08:56:34 +0700

    Update start_brave.sh

--- start_brave.sh
+++ start_brave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm /tmp/.X1-lock
+[ -f "/tmp/.X1-lock" ] && rm "/tmp/.X1-lock"
 USER=root tightvncserver :1 -geometry 1900x900 -depth 24 &
 sleep 3
 #/opt/novnc/utils/novnc_proxy --vnc localhost:5901 --listen 6080 &
